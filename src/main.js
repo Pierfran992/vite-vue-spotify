a9ec5fb8184bea587a7d8612e47b91bd860fa98c
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-02 16:22:39 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faSquareJs } from '@fortawesome/free-brands-svg-icons'
+import { faVuejs } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret, faSquareJs, faVuejs)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
