48507d7e8ade80b5ba7cc623f7a8cdef0a438136
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-05 16:45:42 +0100

    Creazione struttura del componente BannerBottom;

---
+++ src/components/BannerBottom.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "BannerBottom"
+}
+</script>
+
+<template>
+    <div class="ctn_all">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/mixins.scss";
+@use "../styles/partials/variables.scss";
+
+.ctn_all {
+    width: 100vw;
+    height: 80px;
+    background: rgb(135, 0, 133);
+    background: linear-gradient(90deg, rgba(135, 0, 133, 1) 22%, rgba(125, 18, 179, 1) 43%, rgba(68, 16, 213, 1) 74%, rgba(0, 212, 255, 1) 100%);
+}
+</style>
