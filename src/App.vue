41b2fac3434c42fdf24a63fab4b925ddd2830c7b
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-05 16:52:46 +0100

    Richiamati i componenti creati nel componente principale App;

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script>
+import AppBody from "./components/AppBody.vue";
+import BannerBottom from "./components/BannerBottom.vue";
 
+export default {
+    name: "App",
+    components: {
+        AppBody,
+        BannerBottom,
+    }
+}
 </script>
 
 <template>
-
+    <AppBody />
+    <BannerBottom />
 </template>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+@use "./styles/general.scss" as *;
 </style>
