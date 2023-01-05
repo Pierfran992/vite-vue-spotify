ffff418ee0b2ea8eb651397e31e4054f0deac003
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-05 16:50:57 +0100

    Creata la struttura dei due sottocomponeti del corpo principale dell'app;

---
+++ src/components/BarLeft.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "BarLeft",
+}
+</script>
+
+<template>
+    <section>
+
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/mixins.scss";
+@use "../styles/partials/variables.scss";
+
+section {
+    width: 200px;
+    height: 100%;
+    background-color: #000000;
+}
+</style>
