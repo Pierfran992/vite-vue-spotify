b73b8bacaa74aab66a9b5d8184a39d5286104e5b
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-05 16:52:02 +0100

    Creato il componente che conterrà il corpo princiapale dell'app;

---
+++ src/components/AppBody.vue
@@ -0,0 +1,27 @@
+<script>
+import BarLeft from './BarLeft.vue';
+
+export default {
+    name: "AppBody",
+    components: {
+        BarLeft,
+    }
+}
+</script>
+
+<template>
+    <section class="ctn_body_app">
+        <BarLeft />
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/mixins.scss";
+@use "../styles/partials/variables.scss";
+
+.ctn_body_app {
+    width: 100vw;
+    height: calc(100vh - 80px);
+    display: flex;
+}
+</style>
