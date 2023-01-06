c00cb297f21ba2e9890ae981e74de908448a8a32
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-06 11:12:10 +0100

    Completato il componente BannerBottom;

--- src/components/BannerBottom.vue
+++ src/components/BannerBottom.vue
@@ -6,7 +6,17 @@ export default {
 
 <template>
     <div class="ctn_all">
+        <!-- testo del banner -->
+        <div class="ctn_text">
+            <h5>ANTEPRIMA DI SPOTIFY</h5>
+            <p>Iscriviti per ricevere brani e podcast illimitati con annunci pubblicitari occasionali. Non è necessario
+                alcuna carta di credito.</p>
+        </div>
 
+        <!-- bottone banner per iscriversi -->
+        <div class="btn">
+            Iscriviti gratis
+        </div>
     </div>
 </template>
 
@@ -17,7 +27,23 @@ export default {
 .ctn_all {
     width: 100vw;
     height: 80px;
+    padding: 0 15px;
     // background: rgb(135, 0, 133);
     background: $bgBannerBottom;
+    @include between();
+
+    .ctn_text {
+        color: $primaryColor;
+        font-size: 12px;
+
+        p {
+            font-weight: 600;
+            margin-top: 10px;
+        }
+    }
+
+    .btn {
+        @include button();
+    }
 }
 </style>
