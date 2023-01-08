f116092560ae70e2c0a10ef7b286c5f207f0f4c9
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-08 15:29:28 +0100

    Creazione: - sottocomponente AppFooter; - CardGenerator;

---
+++ src/components/list song components/AppFooter.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "AppFooter",
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../../styles/partials/mixins.scss" as *;
+@use "../../styles/partials/variables.scss" as *;
+</style>
---
+++ src/components/list song components/CardGenerator.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "CardGenerator",
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../../styles/partials/mixins.scss" as *;
+@use "../../styles/partials/variables.scss" as *;
+</style>
