bcb606bc82c7aa6385532e55bd3d47669ec04e0f
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-08 19:47:51 +0100

    Sviluppo App: - richiamato lo store; -creato il metodo per far stampare le card delle canzioni;

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <script>
+import axios from 'axios';
 import AppBody from "./components/AppBody.vue";
 import BannerBottom from "./components/BannerBottom.vue";
+
+import { store } from './store.js';
 
 export default {
     name: "App",
     components: {
         AppBody,
         BannerBottom,
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        // creo il metodo per far inserire gli oggetti contenuti in tracks nell'array vuoto songList
+        getSong() {
+
+            let myUrl = store.apiUrl;
+
+            axios
+                .get(myUrl)
+                .then(res => {
+                    this.store.songList = res.data.tracks;
+                })
+                .catch(problem => {
+                    console.log("Errori", problem);
+                });
+        }
+    },
+    mounted() {
+        // faccio partire il metodo dopo che ha caricato la struttura della pagina
+        this.getSong();
+        console.log(store.songList);
     }
 }
 </script>
