13f8c253dda6103e30fce83a8a2621c593bcf332
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-08 19:48:32 +0100

    Inserito l'apiUrl con il link dell'api e un array vuoto;

--- src/store.js
+++ src/store.js
@@ -29,4 +29,6 @@ export const store = reactive({
         "Impostazioni cookie",
         "Info annunci",
     ],
+    apiUrl: "https://shazam.p.rapidapi.com/songs/list-recommendations?rapidapi-key=ac17c959b3msh7946882415fa45ep1ea6cdjsn551ae2916179&key=484129036&locale=en-US",
+    songList: [],
 });
