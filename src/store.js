9b05adca36c23a914247dfa18ed8518afc49d45d
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-08 15:28:13 +0100

    Creazione file store.js (global state): - inseriti i dati da stampare nel componente AppFooter;

---
+++ src/store.js
@@ -0,0 +1,32 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    // dati per il footer
+    azienda: [
+        "Chi siamo",
+        "Oppurtunità di lavoro",
+        "For the Record"
+    ],
+
+    Community: [
+        "Per artisti",
+        "Sviluppatori",
+        "Pubblicità",
+        "Investitori",
+        "Venditori"
+    ],
+
+    linkUtili: [
+        "Assistenza",
+        "App per cellulare gratuita",
+        "Diritti del consumatore"
+    ],
+
+    info: [
+        "Informazioni legali",
+        "Centro sulla privacy",
+        "Informativa sulla privacy",
+        "Impostazioni cookie",
+        "Info annunci",
+    ],
+});
